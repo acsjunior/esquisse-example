0c8a28cc21cf01269703c8c7cdf9dd6dd801da7a
Antonio Carlos da Silva Júnior <acsjunior@acsjunior-macbook.local>
2020-01-11 14:20:01 -0300

    updated script

--- esquisse.R
+++ esquisse.R
@@ -4,7 +4,6 @@
 #
 #   Documentação:
 #     https://dreamrs.github.io/esquisse/index.html
-#     https://cran.r-project.org/web/packages/esquisse/vignettes/get-started.html
 #
 #   Requisito para Mac OSX:
 #     https://www.xquartz.org
@@ -18,7 +17,7 @@
 #--------------------------------------------------------------------------------------------------
 # Instalando e carregando:
 
-install.packages("esquisse")
+# install.packages("esquisse")
 library(esquisse)
 
 #--------------------------------------------------------------------------------------------------
@@ -47,8 +46,33 @@ library(esquisse)
   # Dataset específico
   data(iris)
   esquisser(iris)
+  
+  df <- iris %>%
+    filter(Species == "setosa")
+  
+  head(iris)
 
+  str(iris)
+  
+  ggplot(iris) +
+    aes(x = Species, y = Sepal.Width, fill = Species) +
+    geom_boxplot(alpha = 0.5) +
+    labs(x = "Espécie", title = "Exemplo de Gráfico", subtitle = "Esquisse") +
+    theme_gray()
+  
+  
 
+ 
+  
+#--------------------------------------------------------------------------------------------------
+# Materiais úteis:
+  
+# Repositório no GitHub:
+# https://github.com/juniorssz/esquisse-example
 
-
+# Artigo no Linkedin:
+# https://www.linkedin.com/pulse/interface-drag-drop-para-construção-de-gráficos-r-da-silva-júnior/
+  
+# Get started - CRAN:
+# https://cran.r-project.org/web/packages/esquisse/vignettes/get-started.html
 
